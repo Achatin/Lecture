c0934e88e896927e54ddecd3f5efd118f407da48
sn489yw <simon.nigut@student.tuke.sk>
2023-09-13 18:18:50 +0200

    Rating component upgrade

--- src/components/Rating.tsx
+++ src/components/Rating.tsx
@@ -9,12 +9,20 @@ const Rating: FC<RatingProps> = ({}) => {
   const [rating, setRating] = useState(0);
   const [hoveredRating, setHoveredRating] = useState(0);
 
+  const ratingLabels = {
+    1: 'Poor',
+    2: 'Fair',
+    3: 'Average',
+    4: 'Good',
+    5: 'Very Good',
+  };
+
   const handleStarClick = (newRating) => {
     setRating(newRating);
   };
 
   return (
-    <div className='flex space-x-0.5'>
+    <div className='flex items-center space-x-0.5'>
       {[...Array(5)].map((_, index) => {
         const starValue = index + 1;
         return (
@@ -25,10 +33,11 @@ const Rating: FC<RatingProps> = ({}) => {
             onMouseLeave={() => setHoveredRating(0)}
             className='cursor-pointer'
           >
-            <Star strokeWidth={1.5} className={`text-muted-foreground ${starValue <= hoveredRating || rating ? 'fill-primary text-primary' : ''}`} />
+            <Star strokeWidth={1.5} className={`text-muted-foreground ${starValue <= hoveredRating || rating ? 'fill-primary text-primary' : 'fill-none'}`} />
           </div>
         );
       })}
+      <p className={`${rating === hoveredRating || (hoveredRating === 0 && rating) ? 'text-primary' : 'text-muted-foreground'} pl-3`}>{ratingLabels[hoveredRating] || ratingLabels[rating] || ''}</p>
     </div>
   );
 }
